26508ff4ed9beecf50939a6cf88cf9c3931e0b1a
Dipanshu Chaubey <dipanshuchaubey@gmail.com>
2020-10-17 16:47:11 +0530

    UI - Get Data from API | Apply Filter

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import Filters from './components/Filters'
+import axios from 'axios'
 import './index.css'
 
 function App() {
-  return <div className="App"></div>
+  const [data, setData] = useState([])
+
+  const getData = async (launchSuccess, landSuccess, launchYear) => {
+    await axios
+      .get(
+        `https://api.spacexdata.com/v3/launches?limit=100&&launch_success=${
+          launchSuccess || ''
+        }&&land_success=${landSuccess || ''}&&launch_year=${launchYear || ''}`
+      )
+      .then(data => setData(data.data))
+      .catch(err => console.log(err))
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+
+  return (
+    <div className="App">
+      <div className="container">
+        <h1 className="main-heading">SpaceX Launch Programs</h1>
+
+        <Filters
+          cb={val =>
+            getData(val.launchSuccess, val.landSuccess, val.launchYear)
+          }
+        />
+      </div>
+    </div>
+  )
 }
 
 export default App
